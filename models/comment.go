c6935c6eb2966f2668954347fe8dc081ce6d14f4
adruzhkin <andrey.druzhkin@gmail.com>
2026-03-21 12:16:07 -0600

    Remove unused Descendants field from Story and Comment structs

--- models/comment.go
+++ models/comment.go
@@ -4,7 +4,6 @@ type Comment struct {
 	IsDeleted   bool   `json:"deleted"`
 	ID          int    `json:"id"`
 	CreatedBy   string `json:"by"`
-	Descendants int    `json:"descendants"`
 	Kids        []int  `json:"kids"`
 	Parent      int    `json:"parent"`
 }
--- models/story.go
+++ models/story.go
@@ -3,7 +3,6 @@ package models
 type Story struct {
 	ID          int    `json:"id"`
 	CreatedBy   string `json:"by"`
-	Descendants int    `json:"descendants"`
 	Kids        []int  `json:"kids"`
 	Title       string `json:"title"`
 }
